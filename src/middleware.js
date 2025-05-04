109f52ca3296b6b5a53859f2ccb470e86867e5e3
Nishant04-dev <info.nishantchauhan@gmail.com>
2025-05-05 00:47:01 +0530

    Add files via upload

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "722f0e6e-610c-4427-ac3d-5b8cab467fe5");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
